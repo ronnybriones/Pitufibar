07253553c8095355ec9f75bab73dfd699f582ec4
ronnybriones <ronny13200312@gmail.com>
2024-06-08 04:30:12 +0000

    pituficagada

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render # type: ignore
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello, world. You're at the home page.")
 # Create your views here.
 def home (request):
     return render(request, 'home.html')
